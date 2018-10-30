2018c7fe8d42993be448f517aafd13e95a822cd9
Albedich <44615116+Albedich@users.noreply.github.com>
2018-10-30 22:18:32 +0400

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1540923216,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/boy-sheet0.png",
+		"images/boy-sheet1.png",
+		"images/boy-sheet2.png",
+		"images/one-sheet0.png",
+		"images/two-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/head-sheet0.png",
+		"images/marker_destroy-sheet0.png",
+		"images/stomachbg-sheet0.png",
+		"images/pause-sheet0.png",
+		"images/sampleship-sheet0.png",
+		"images/meteorite_large-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiled_death.png",
+		"images/solid_floor.png",
+		"images/play-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
